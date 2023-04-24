36e8caf31144022f1be496357976506918272705
Marcus Morrow <marcusmorrow319@gmail.com>
2023-04-24 17:52:15 -0500

    changed static root

--- my_copy_portfolio/settings.py
+++ my_copy_portfolio/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'my_copy_portfolio/static/')]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
